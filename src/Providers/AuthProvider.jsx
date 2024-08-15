6350ac456473864d278d375d6f969ea1aff6322b
Fardin15 <mohammedshowrav15@gmail.com>
2024-08-16 00:00:31 +0600

    authprovider created

---
+++ src/Providers/AuthProvider.jsx
@@ -0,0 +1,90 @@
+import { createContext, useEffect, useState } from "react";
+import PropTypes from "prop-types";
+import {
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+import app from "../firebase/firebase.config";
+
+const auth = getAuth(app);
+export const AuthContext = createContext(null);
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const googleProvider = new GoogleAuthProvider();
+
+  //   createUser
+  const createUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  //   signIn
+  const signIn = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  // sign in with google
+  const signInWithGoogle = () => {
+    setLoading(true);
+    return signInWithPopup(auth, googleProvider);
+  };
+
+  //   logOut
+  const logOut = () => {
+    setLoading(true);
+    return signOut(auth);
+  };
+
+  //   onAuthStateChanged
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      console.log(currentUser);
+      setUser(currentUser);
+      if (currentUser) {
+        // get token and store client
+        const userInfo = { email: currentUser.email };
+        //     axiosPublic.post("/jwt", userInfo).then((res) => {
+        //       if (res.data.token) {
+        //         localStorage.setItem("access-token", res.data.token);
+        //         setLoading(false);
+        //       }
+        //     });
+        //   } else {
+        // remove token and remove client
+        // localStorage.removeItem("access-token");
+        // setLoading(false);
+      }
+    });
+    return () => {
+      return unsubscribe();
+    };
+  }, []);
+
+  const authInfo = {
+    user,
+    setUser,
+    loading,
+    setLoading,
+    createUser,
+    signIn,
+    logOut,
+    signInWithGoogle,
+  };
+
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
+
+AuthProvider.propTypes = {
+  children: PropTypes.node,
+};
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBMLF5KrRwD7WbQQ7VQ8WpNz3z2fKa2CEE",
+  authDomain: "foodie-go-f531b.firebaseapp.com",
+  projectId: "foodie-go-f531b",
+  storageBucket: "foodie-go-f531b.appspot.com",
+  messagingSenderId: "626821160888",
+  appId: "1:626821160888:web:bb3fb4c6654827ddecf94d",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;

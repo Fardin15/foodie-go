28b3656a314064414c29964b7e5aa9ac6c45546e
Fardin15 <mohammedshowrav15@gmail.com>
2024-08-16 11:07:11 +0600

    register route created

--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -1,7 +1,17 @@
+import { Outlet, useLocation } from "react-router-dom";
+import Navbar from "../Components/Navbar/Navbar";
+import Footer from "../Components/Footer/Footer";
+
 const Main = () => {
+  const location = useLocation();
+  const noHeaderFooter =
+    location.pathname.includes("login") ||
+    location.pathname.includes("register");
   return (
-    <div>
-      <h1>Main</h1>
+    <div className="container max-w-6xl mx-auto">
+      {noHeaderFooter || <Navbar></Navbar>}
+      <Outlet></Outlet>
+      {noHeaderFooter || <Footer></Footer>}
     </div>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,23 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Main from "./Main/Main";
+import Home from "./Home/Home";
+import Register from "./Register/Register";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Main></Main>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "register",
+        element: <Register></Register>,
+      },
+    ],
   },
 ]);
 
